1a717d2a1eabc9eaf85e51508acc5d6acdf871f8
kanade0404 <melty0404@gmail.com>
2021-10-30 21:58:24 +0900

    プロフィール取得API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/login', [\App\Http\Controllers\LoginController::class, "login"]);
+Route::post('/logout', [\App\Http\Controllers\LoginController::class, "logout"]);
+Route::get('/user/{id}', [\App\Http\Controllers\MeController::class, "index"]);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    //
+});
