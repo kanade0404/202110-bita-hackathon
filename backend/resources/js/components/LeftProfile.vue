afc682c34579277814a253568e7fabd57c570881
工藤昂大 <kudokodai@bitA01202.local>
2021-10-30 17:52:01 +0900

    commit

---
+++ backend/resources/js/components/LeftProfile.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>
+      <div class="profileImg"><img :src="profileInfo.thumbnail_path"></div>
+      <div class="centerItem">{{ profileInfo.userName }}</div>
+    </div>
+    <div>
+      <ul>
+        <li v-for="clubName in profileInfo.clubNames" :key="clubName"  class="centerItem">{{ clubName }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LeftProfile',
+  props: {
+    profileInfo: {
+      type: Object,
+      required: true,
+      default: () => {},
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.centerItem {
+  margin: 0 auto;
+}
+</style>
--- backend/resources/js/components/top.vue
+++ backend/resources/js/components/top.vue
@@ -1,118 +1,44 @@
 <template>
   <div class="wrapper">
-    <div class="profileArea areaBorder">
-      <div>
-        <div class="profileImg"><img :src="profileImg"></div>
-        <div>名前名前</div>
-      </div>
-      <div>
-        <ul>
-          <li>部活名</li>
-          <li>部活名</li>
-          <li>部活名</li>
-        </ul>
-      </div>
+    <div class="left">
+      <LeftProfile />
     </div>
-    <div class="timeLine">
-      <ul>
-        <li class="clubCasset">
-          <div>
-            <div>部活名</div>
-            <div>参加者募集</div>
-          </div>
-          <p>
-            お疲れ様です！！
-            <br>最近、ボドゲ会は平日に行うことが多いですが、たまには休日のまとまった時間でボドゲしましょう！！
-            <br>日程の入力をお願いしますm(_ _)m
-          </p>
-        </li>
-        <li class="clubCasset">
-          <div>
-            <div>部活名</div>
-            <div>参加者募集</div>
-          </div>
-          <p>
-            お疲れ様です！！
-            <br>最近、ボドゲ会は平日に行うことが多いですが、たまには休日のまとまった時間でボドゲしましょう！！
-            <br>日程の入力をお願いしますm(_ _)m
-          </p>
-        </li>
-        <li class="clubCasset">
-          <div>
-            <div>部活名</div>
-            <div>参加者募集</div>
-          </div>
-          <p>
-            お疲れ様です！！
-            <br>最近、ボドゲ会は平日に行うことが多いですが、たまには休日のまとまった時間でボドゲしましょう！！
-            <br>日程の入力をお願いしますm(_ _)m
-          </p>
-        </li>
-        <li class="clubCasset">
-          <div>
-            <div>部活名</div>
-            <div>参加者募集</div>
-          </div>
-          <p>
-            お疲れ様です！！
-            <br>最近、ボドゲ会は平日に行うことが多いですが、たまには休日のまとまった時間でボドゲしましょう！！
-            <br>日程の入力をお願いしますm(_ _)m
-          </p>
-        </li>
-      </ul>
+    <div class="center">
+      <Center />
     </div>
-    <div class="">
-      <div>決まった部活</div>
-      <ul>
-        <li><div>日程</div><div>部活名部活名</div></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+    <div class="right">
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import LeftProfile from './LeftProfile.vue';
+import Center from './Center.vue';
+
 export default {
+  components: {
+    LeftProfile,
+    Center,
+  },
   data() {
     return {
-      profileImg: 'https://mikan.bita.jp/img/members_img/15656647750seiya_sakata_mikan.jpg',
+      profileInfo: {
+        thumbnail_path: 'https://mikan.bita.jp/img/members_img/15656647750seiya_sakata_mikan.jpg',
+        userName: '',
+        clubNames: ['部活名', '部活名', '部活名', '部活名'],
+      },
     };
   },
 }
 </script>
 
 <style lang="scss" scoped>
-@import '../../sass/reset';
-
 .wrapper {
   display: flex;
   justify-content: space-around;
-  .profileArea {
-    .profileImg {
-      position:relative;
-      overflow: hidden;
-      width:100px;
-      height:100px;
-      border-radius:50%;
-      & img{
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        height:100%;
-      }
-    }
-  }
-  .timeLine {
-    width: 500px;
-    .clubCasset {
-      margin: 10px 0;
-      border-radius: 5px;
-      padding: 12px 16px;
-      box-shadow: 0 1px 5px #d3d4d4;
-    }
+  .center {
+    width: 500px
   }
 }
 </style>
